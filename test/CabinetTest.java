7a39313047632a06ffe2fb5b5081dfac5ea5bbee
jnickg <jnickg83@gmail.com>
2013-09-07 12:55:03 -0700

    Make a test script to make sure basic operations work
    *Added Package test
    *Added Class CabinetTest to Package test
    
    Currently working on using script to improve code

---
+++ test/CabinetTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.Collection;
+
+import cabinet.*;
+
+public class CabinetTest {
+	
+	private static File			homedir		= new File("C:\\Users\\Nick\\Desktop\\TestFldr");
+	private static Cabinet		cab			= new Cabinet(homedir, "Cabinet");
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

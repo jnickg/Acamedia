7bec90a7681cfb79f0f0b6d93bfb4b20721d820b
jnickg <jnickg83@gmail.com>
2013-09-15 14:23:15 -0700

    Move CabinetTest to default package
    still debugging ClassNotFoundException / NoClassDefFoundError

--- CabinetTest.java
+++ CabinetTest.java
@@ -1,5 +1,3 @@
-package test;
-
 import cabinet.*;
 import java.io.File;
 import java.io.PrintStream;
@@ -8,10 +6,13 @@ public class CabinetTest
 {
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		//Item			item;
 		PrintStream		out		= System.out;	// we will be printing to the standard output stream
 		File				homedir	= new File("C:\\Users\\Nick\\Desktop\\TestFldr"); // test folder on the desktop
+		
+		out.println("CLASSPATH: " + System.getProperty("java.class.path"));
 		
 		Cabinet	cab	= new Cabinet(homedir, "cab", out);
 		cab.printAll(out);

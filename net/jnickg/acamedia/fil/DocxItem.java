00ea5d602e394e4980165f3b62cf33b173867f09
jnickg <jnickg83@gmail.com>
2013-09-27 17:09:48 -0700

    Add DocxItem class to net.jnickg.acamedia.fil
    Used for DOCX file formats

---
+++ net/jnickg/acamedia/fil/DocxItem.java
@@ -0,0 +1,12 @@
+package net.jnickg.acamedia.fil;
+
+import java.io.File;
+
+public class DocxItem extends Item {
+
+	public DocxItem(File f) {
+		super(f);
+		// TODO Auto-generated constructor stub
+	}
+
+}

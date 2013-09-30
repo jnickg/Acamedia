5917f5bc44ae06fbd4928262125711510abac0b2
jnickg <jnickg83@gmail.com>
2013-09-29 17:12:23 -0700

    Added class for what will become GUI

---
+++ net/jnickg/acamedia/Acamedia.java
@@ -0,0 +1,14 @@
+package net.jnickg.acamedia;
+
+public class Acamedia {
+
+	private Acamedia() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
